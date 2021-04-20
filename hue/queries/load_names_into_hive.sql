6f367087ecd5e4c95dd65e9a5e52b91671dc834b
kevinsuedmersen <kevinsuedmersen@gmail.com>
2021-04-20 21:46:37 +0200

    loading movie names into hive

---
+++ hue/queries/load_names_into_hive.sql
@@ -0,0 +1,30 @@
+create table if not exists names(
+    movie_id int,
+    title int,
+    airing_date date,
+    col1 int,
+    col2 int,
+    col3 int,
+    col4 int,
+    col5 int,
+    col6 int,
+    col7 int,
+    col8 int,
+    col9 int,
+    col10 int,
+    col11 int,
+    col12 int,
+    col13 int,
+    col14 int,
+    col15 int,
+    col16 int,
+    col17 int,
+    col18 int,
+    col19 int)
+row format delimited
+fields terminated by '|'
+stored as textfile;
+
+
+load data inpath  '/user/root/playground/ml-100k/u.item'
+overwrite into table names;
